85bacd05b7911643bffcec0c239d03927d6549c4
JP Dhabolt <john.p.dhabolt@gmail.com>
2022-03-20 21:53:43 +0000

    Rename package.json in template to resolve lint issue

--- packages/create/src/after.ts
+++ packages/create/src/after.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
+import fs from 'fs';
 import { resolve } from 'path';
 import { AfterHookOptions } from 'create-create-app';
 
@@ -11,18 +12,15 @@ function cleanupCrapThatCreateAppCliGeneratesWithoutAsking({
     cwd: resolve(packageDir, '..', '..'),
   });
 
-  run('rm -rf .git/', {
-    cwd: packageDir,
-  });
-
-  run('rm yarn.lock', {
-    cwd: packageDir,
-  });
+  fs.rmdirSync(resolve(packageDir, '.git'));
+  fs.unlinkSync(resolve(packageDir, 'yarn.lock'));
 }
 
-export function after({ run, packageDir, answers }: AfterHookOptions): void {
+export async function after({ run, packageDir, answers }: AfterHookOptions): Promise<void> {
   if (answers.isGwyddion) {
     cleanupCrapThatCreateAppCliGeneratesWithoutAsking({ run, packageDir });
   }
+
+  fs.renameSync(resolve(packageDir, 'package.json.handlebars'), resolve(packageDir, 'package.json'));
   console.log(`Your package is available at ${packageDir}.  Happy coding!`);
 }
