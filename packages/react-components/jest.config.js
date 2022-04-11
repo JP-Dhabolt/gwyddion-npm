99040be963d0d2ade8dde9a36755ebba048725b7
JP Dhabolt <john.p.dhabolt@gmail.com>
2022-04-11 00:41:00 +0000

    Work in progress for react libs

---
+++ packages/react-components/jest.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+const base = require('@gwyddion/config/files/jest.config');
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  ...base,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
